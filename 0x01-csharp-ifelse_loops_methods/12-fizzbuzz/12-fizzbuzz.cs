ec09efe41d83953d2ebfe1acd28765e15c80c702
ranicholson <ranicho2@gmail.com>
2021-07-13 17:58:12 +0000

    Task 12

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int count = 1; count <= 100; count++)
+            {
+                if (count % 3 == 0 && count % 5 == 0)
+                {
+                        Console.Write("FizzBuzz "); 
+                }
+                else if (count % 5 == 0)
+                {
+                    if (count == 100)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        Console.Write("Buzz ");
+                    }
+                }
+                else if (count % 3 == 0)
+                {
+                    Console.Write("Fizz " );
+                }
+                else
+                {
+                    Console.Write("{0} ", count);
+                }
+            }
+        }
+    }
+}
