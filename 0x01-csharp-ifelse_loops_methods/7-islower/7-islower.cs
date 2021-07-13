48258ebbabbb94870deee6bcb280214876a67b25
ranicholson <ranicho2@gmail.com>
2021-07-13 17:17:55 +0000

    Task 7

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    class Character
+    {
+        public static bool IsLower(char c)
+        {
+            if (c <= 'z' && c >= 'a')
+            {
+                return (true);
+            }
+            else
+            {
+                return (false);
+            }
+        }
+    }
