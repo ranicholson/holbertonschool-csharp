c28537f2940f126613d58af9588e0f2d29938d42
ranicholson <ranicho2@gmail.com>
2021-07-13 16:59:03 +0000

    Task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int first = 0; first < 10; first++)
+            {
+                for (int second = first + 1; second < 10; second++)
+                {
+                    if (first == 8 && second == 9)
+                    {
+                        Console.WriteLine("89");
+                        break;
+                    }
+                    Console.Write("{0}{1}, ", first, second);
+                }
+            }
+        }
+    }
+}
