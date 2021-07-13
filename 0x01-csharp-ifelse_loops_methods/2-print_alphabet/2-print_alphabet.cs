a1be8f55c0416f92890099090401b1fb08670dcb
ranicholson <ranicho2@gmail.com>
2021-07-13 16:24:03 +0000

    Task 2

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = 'a';
+            while (letter <= 'z')
+            {
+                Console.Write("{0}", letter);
+                letter++;
+            }
+
+        }
+    }
+}
