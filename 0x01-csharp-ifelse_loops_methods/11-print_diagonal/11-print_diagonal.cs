18737c8e868b48c74344bbca4d686537c47dcc81
ranicholson <ranicho2@gmail.com>
2021-07-13 17:47:37 +0000

    Task 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    class Line
+    {
+        public static void PrintDiagonal(int length)
+        {
+            if (length <= 0)
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                for (int space = 0; space <= length; space++)
+                {
+                    string spaces = new String(' ', space);
+                    Console.WriteLine("{0}\\", spaces);
+                }
+            }
+        }
+    }
