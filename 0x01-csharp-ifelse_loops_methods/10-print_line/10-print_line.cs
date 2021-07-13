850ba5c98b229aef21e6798e3db3f567e051ce94
ranicholson <ranicho2@gmail.com>
2021-07-13 17:37:30 +0000

    Task 10

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    class Line
+    {
+        public static void PrintLine(int length)
+        {
+            if (length <= 0)
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                for (int x = 0; x <= length; x++)
+                {
+                    Console.Write("_");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
