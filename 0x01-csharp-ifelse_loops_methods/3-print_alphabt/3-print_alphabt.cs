da8494a872df9a2192798f5a4d6a7a0bd62c4c2b
ranicholson <ranicho2@gmail.com>
2021-07-13 16:36:08 +0000

    Task 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = 'a';
+            while (letter <= 'z')
+            {
+                if (letter == 'e' || letter == 'q')
+                {
+                    letter++;
+                    continue;
+                }
+                Console.Write("{0}", letter);
+                letter++;
+            }
+
+        }
+    }
+}
