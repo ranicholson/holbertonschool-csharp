bdcd49dc43b91da81588cca76091fd589e4cde67
ranicholson <ranicho2@gmail.com>
2021-07-13 16:48:06 +0000

    Task 5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 0; number <= 99; number++)
+            {
+                if (number == 99)
+                {
+                    Console.WriteLine("99");
+                    break;
+                }
+                Console.Write("{0:d2}, ", number);
+            }
+        }
+    }
+}
