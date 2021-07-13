0f5c7b753cc09503724d81119a5fd2a39f756687
ranicholson <ranicho2@gmail.com>
2021-07-13 16:41:27 +0000

    Task 4

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            while (number <= 98)
+            {
+                Console.WriteLine("{0} = 0x{0:x}", number);
+                number++;
+            }
+        }
+    }
+}
