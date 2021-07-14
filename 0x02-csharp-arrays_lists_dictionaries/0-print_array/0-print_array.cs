68ce076aded89a69a7efe84bd39e706a1ee3b150
ranicholson <ranicho2@gmail.com>
2021-07-14 15:18:48 +0000

    Task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+    class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return (null);
+            }
+            else
+            {
+                int[] newArray = new int[size];
+                if (size == 0)
+            {
+                Console.WriteLine();
+                return (newArray);
+            }
+                for (int x = 0; x < size; x++)
+                {
+                    newArray[x] = x;
+                    if (x == size - 1)
+                    {
+                        Console.WriteLine(x);
+                        break;
+                    }
+                    Console.Write("{0} ", x);
+                }
+                return (newArray);
+            }
+        }
+    }
---
+++ 0x02-csharp-arrays_lists_dictionaries/README.md
@@ -0,0 +1,11 @@
+# 0x02. C# - Data Structures: Arrays, Lists, Dictionaries
+
+The journey into C# continues.
+
+## How can you practice this same project?
+
+Take a look at the AR/VR specialization at Holberton.
+
+## Questions or Comments
+
+[Allen Nicholson](https://github.com/ranicholson)
