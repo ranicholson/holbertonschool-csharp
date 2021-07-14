74522d3c7fa55695aed6a17a7b2c936969d18b74
ranicholson <ranicho2@gmail.com>
2021-07-14 17:19:06 +0000

    Task 8

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Dictionary
+    {
+        public static int NumberOfKeys(Dictionary<string, string> myDict)
+        {
+            int keyCount = 0;
+            foreach (KeyValuePair<string, string> keys in myDict)
+            {
+                keyCount++;
+            }
+            return (keyCount);
+        }
+    }
