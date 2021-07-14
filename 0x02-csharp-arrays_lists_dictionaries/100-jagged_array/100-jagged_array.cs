4c0df24a0f3b0d4bd4f916e7a2980f1127cc207c
ranicholson <ranicho2@gmail.com>
2021-07-14 19:11:05 +0000

    Task 16

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [][] jaggedArray = new int[][]
+        {
+            new int[] {0, 1, 2, 3},
+            new int[] {0, 1, 2, 3, 4, 5, 6},
+            new int[] {0,1}
+        };
+        for (int x = 0; x < jaggedArray.Length; x++)
+        {
+            for (int y = 0; y < jaggedArray[x].Length; y++)
+            {
+                if (y == jaggedArray[x].Length - 1)
+                {
+                    Console.WriteLine(jaggedArray[x][y]);
+                    break;
+                }
+                Console.Write("{0} ", jaggedArray[x][y]);
+            }
+        }
+        }
+    }
+}
