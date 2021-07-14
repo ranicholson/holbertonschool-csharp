b134ae1dc71725ac67883de07cfa3e5393a5b6ac
ranicholson <ranicho2@gmail.com>
2021-07-14 16:47:10 +0000

    Task 6

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<bool> DivisibleBy2(List<int> myList)
+        {
+            List<bool> booleanList = new List<bool>();
+            foreach(int number in myList)
+            {
+                if (number % 2 == 0)
+                {
+                    booleanList.Add(true);
+                }
+                else
+                {
+                    booleanList.Add(false);
+                }
+            }
+            return (booleanList);
+        }
+    }
