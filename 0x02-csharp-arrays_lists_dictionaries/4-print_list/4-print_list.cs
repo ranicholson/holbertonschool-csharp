8cc0f0e51830451f495d1c01d713f28ed8a3e005
ranicholson <ranicho2@gmail.com>
2021-07-14 16:04:34 +0000

    Task 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> CreatePrint(int size)
+        {
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return (null);
+            }
+            List<int> integerList = new List<int>();
+            for (int x = 0; x < size; x++)
+            {
+                integerList.Add(x);
+            }
+            Console.WriteLine(string.Join(" ", integerList));
+            return (integerList);
+        }
+    }
