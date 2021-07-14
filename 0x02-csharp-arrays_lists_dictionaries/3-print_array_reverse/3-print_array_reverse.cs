8f65742390edbc4b33cee9115d17ffa37d9d78da
ranicholson <ranicho2@gmail.com>
2021-07-14 15:52:20 +0000

    Task 3

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+    class Array
+    {
+        public static void Reverse(int[] array)
+        {
+            if (array != null)
+            {
+                for (int x = array.Length - 1; x >= 0; x--)
+                {
+                    if (x == 0)
+                    {
+                        Console.Write(array[x]);
+                        break;
+                    }
+                    Console.Write("{0} ", array[x]);
+                    }
+            }
+            Console.WriteLine();
+        }
+    }
