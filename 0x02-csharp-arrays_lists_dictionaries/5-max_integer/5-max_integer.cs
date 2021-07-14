3b9ed9e387bceaaec2400d1c14b1074aa0d4ae57
ranicholson <ranicho2@gmail.com>
2021-07-14 16:29:11 +0000

    Task 5

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static int MaxInteger(List<int> myList)
+        {
+            if (myList.Count > 0)
+            {
+                int largestElement = myList[0];
+                for (int x = 0; x < myList.Count; x++)
+                {
+                    if (myList[x] > largestElement)
+                    {
+                        largestElement = myList[x];
+                    }
+                }
+                return (largestElement);
+            }
+            else
+            {
+                Console.WriteLine("List is empty");
+                return (-1);
+            }
+        }
+    }
