5151d039dbbbe068dcf410fa38ffda35f186c29d
ranicholson <ranicho2@gmail.com>
2021-07-14 18:41:15 +0000

    Task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] twodarray = new int[5,5];
+            twodarray [2,2] = 1;
+            for (int firstdimension = 0; firstdimension < 5; firstdimension++)
+            {
+                for (int seconddimension = 0; seconddimension < 5; seconddimension++)
+                {
+                    if (seconddimension == 4)
+                    {
+                        Console.WriteLine("0");
+                        break;
+                    }
+                    Console.Write("{0} ", twodarray[firstdimension,seconddimension]);
+                }
+            }
+        }
+    }
+}
