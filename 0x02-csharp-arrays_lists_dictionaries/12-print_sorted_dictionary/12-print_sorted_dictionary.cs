5e582b46132ee1e33a580e2d70c0039e31a819e7
ranicholson <ranicho2@gmail.com>
2021-07-14 17:58:55 +0000

    Task 12

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Dictionary
+    {
+        public static void PrintSorted(Dictionary<string, string> myDict)
+        {
+            SortedList<string, string> keyOrdered = new SortedList<string, string>();
+            foreach (string key in myDict.Keys)
+            {
+                keyOrdered.Add(key, myDict[key]);
+            }
+            foreach (string key in keyOrdered.Keys)
+            {
+                Console.WriteLine("{0}: {1}", key, keyOrdered[key]);
+            }
+        }
+    }
