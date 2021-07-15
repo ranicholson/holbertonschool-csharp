e1673d82de98b4c184bfbd5233dc58b14f2e1df8
ranicholson <ranicho2@gmail.com>
2021-07-15 16:59:37 +0000

    Task 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static int Sum(List<int> myList)
+        {
+            HashSet<int> uniqueInts = new HashSet<int>(myList);
+            int uniqueSum = 0;
+            foreach (int x in uniqueInts)
+            {
+                uniqueSum += x;
+            }
+            return (uniqueSum);
+        }
+    }
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/README.md
@@ -0,0 +1,11 @@
+# 0x03. C# - Data Structures: HashSet, Stack, Queue, LinkedList
+
+Prying loose the secrets of C#.
+
+## How can you practice this same project?
+
+Take a look at the AR/VR specialization at Holberton.
+
+## Questions or Comments
+
+[Allen Nicholson](https://github.com/ranicholson)
