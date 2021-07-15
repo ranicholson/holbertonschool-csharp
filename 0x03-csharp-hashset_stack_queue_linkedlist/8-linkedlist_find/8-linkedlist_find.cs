a5f88a119109c1e88423c318fbfce1efb4b3a71f
ranicholson <ranicho2@gmail.com>
2021-07-15 19:59:46 +0000

    Task 8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        if (myLList.Contains(value) == false)
+        {
+            return (-1);
+        }
+        int listIndex = 0;
+        foreach (int storedValue in myLList)
+        {
+            if (storedValue == value)
+            {
+                return (listIndex);
+            }
+            listIndex++;
+        }
+        return (-1);
+    }
+}
