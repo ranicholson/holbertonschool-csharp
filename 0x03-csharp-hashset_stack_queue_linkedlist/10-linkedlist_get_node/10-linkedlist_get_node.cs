756042c557040de333db32b452548ac8360be8af
ranicholson <ranicho2@gmail.com>
2021-07-15 20:18:12 +0000

    Task 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class LList
+    {
+        public static int GetNode(LinkedList<int> myLList, int n)
+        {
+            if (myLList.Count <= n)
+            {
+                return (0);
+            }
+            int nodeIndex = 0;
+            foreach (int storedValue in myLList)
+            {
+                if (nodeIndex == n)
+                {
+                    return (storedValue);
+                }
+                nodeIndex++;
+            }
+            return (0);
+        }
+    }
