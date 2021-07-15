63713be89c763c21a7d1d91928ffabbf5a455280
ranicholson <ranicho2@gmail.com>
2021-07-15 20:49:28 +0000

    Task 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> killNode = myLList.First;
+        int indexTracker = 0;
+        while (killNode != null)
+        {
+            if (indexTracker == index)
+            {
+                myLList.Remove(killNode);
+                break;
+            }
+            killNode = killNode.Next;
+            indexTracker++;
+        }
+    }
+}
