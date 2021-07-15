0eee9406297f7f02ad643141fac064d039d7fde0
ranicholson <ranicho2@gmail.com>
2021-07-15 17:33:46 +0000

    Task 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> DifferentElements(List<int> list1, List<int> list2)
+        {
+            HashSet<int> sortedCommonElements = new HashSet<int>(list1);
+            sortedCommonElements.SymmetricExceptWith(list2);
+            List<int> returnList = new List<int>();
+            foreach (int item in sortedCommonElements)
+            {
+                returnList.Add(item);
+            }
+            returnList.Sort();
+            return (returnList);
+        }
+    }
