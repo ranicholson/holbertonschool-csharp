7eff4427659cca6f1d07036f0ef3e757af28a564
ranicholson <ranicho2@gmail.com>
2021-07-15 19:11:20 +0000

    Task 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> integerLinkList = new LinkedList<int>();
+        if (size < 0)
+        {
+            return (integerLinkList);
+        }
+        for (int x = 0; x < size; x++)
+        {
+            integerLinkList.AddLast(x);
+            Console.WriteLine(x);
+        }
+        return (integerLinkList);
+    }
+}
