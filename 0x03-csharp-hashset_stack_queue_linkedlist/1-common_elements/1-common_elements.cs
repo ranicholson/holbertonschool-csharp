cf3e31e2292d757b4df4918cd789ceca056e1bcc
ranicholson <ranicho2@gmail.com>
2021-07-15 17:24:31 +0000

    Task 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> CommonElements(List<int> list1, List<int> list2)
+        {
+            HashSet<int> sortedCommonElements = new HashSet<int>(list1);
+            sortedCommonElements.IntersectWith(list2);
+            List<int> returnList = new List<int>();
+            foreach (int item in sortedCommonElements)
+            {
+                returnList.Add(item);
+            }
+            return (returnList);
+        }
+    }
