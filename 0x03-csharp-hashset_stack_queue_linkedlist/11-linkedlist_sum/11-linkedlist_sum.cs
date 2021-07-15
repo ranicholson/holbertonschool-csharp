1b61aa2fdf0b7548cca01ffd59046fccf4433f03
ranicholson <ranicho2@gmail.com>
2021-07-15 20:22:09 +0000

    Task 11

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int dataSum = 0;
+        foreach (int storedValue in myLList)
+        {
+            dataSum += storedValue;
+        }
+        return (dataSum);
+    }
+}
