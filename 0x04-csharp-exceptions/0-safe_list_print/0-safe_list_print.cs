b28f10c9153ddfc72a45316bea8bedab0473481c
ranicholson <ranicho2@gmail.com>
2021-07-26 14:47:10 +0000

    Task 0 and README

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int numToPrint = 0;
+        int numPrinted = 0;
+        try
+        {
+            numToPrint = myList[n];
+            numToPrint = n - 1;
+        }
+        catch (Exception)
+        {
+            numToPrint = myList.Count - 1;
+        }
+        finally
+        {
+            for (; numPrinted <= numToPrint; numPrinted++)
+            {
+                Console.WriteLine(myList[numPrinted]);
+            }
+        }
+        return (numPrinted);
+    }
+}
