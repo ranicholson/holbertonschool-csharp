d912bb58a969cae770bb6dfb0396aed53ff3c5f0
ranicholson <ranicho2@gmail.com>
2021-07-26 17:02:17 +0000

    Task 3

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,27 @@
+﻿enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string newName, float newAge, string newOwner, Rating newRating)
+    {
+        name = newName;
+        age = newAge;
+        owner = newOwner;
+        rating = newRating;
+    }
+
+    public override string ToString()
+    {
+        return "Dog Name: " + name + "\nAge: " + age + "\nOwner: " + owner + "\nRating: " + rating;
+    }
+}
