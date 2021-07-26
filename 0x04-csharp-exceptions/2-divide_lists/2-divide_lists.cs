5c131e7f798fbad6b1f5de0da46ef23bc131d849
ranicholson <ranicho2@gmail.com>
2021-07-26 15:10:27 +0000

    Task 2

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> quotientList = new List<int>();
+
+        for (int x = 0; x < listLength; x++)
+        {
+            try
+            {
+                quotientList.Add(list1[x] / list2[x]);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                quotientList.Add(0);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+        return (quotientList);
+    }
+}
