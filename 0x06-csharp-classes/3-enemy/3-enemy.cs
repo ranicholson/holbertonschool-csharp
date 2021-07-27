fc9588e40eafcf3fe1c4a3397fce86de1329acdf
ranicholson <ranicho2@gmail.com>
2021-07-27 17:45:40 +0000

    Task 3

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Class Zombie under the Enemies namespace.</summary>
+    class Zombie
+    {
+        ///<summary>Health field that has value assigned when new instance is initialized</summary>
+        private int health;
+
+        ///<summary>Initializes new instance of Zombie class.</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        ///<summary>Initializes new instance of class Zombie with value to be assigned to health</summary>
+        ///<param name="value">Value to set health. Must be greater than or equal to zero.</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+        }
+
+        ///<summary>Returns the value of health</summary>
+        public int GetHealth()
+        {
+            return (this.health);
+        }
+
+    }
+}
