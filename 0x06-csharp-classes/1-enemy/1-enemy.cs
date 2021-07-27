5144c47b31712011dd8549673428e08ba1406076
ranicholson <ranicho2@gmail.com>
2021-07-27 17:32:15 +0000

    Task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Class Zombie under the Enemies namespace.</summary>
+    class Zombie
+    {
+        ///<summary>Health field that has value assigned when new instance is initialized</summary>
+        public int health;
+
+        ///<summary>Initializes new instance of Zombie class.</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+    }
+}
