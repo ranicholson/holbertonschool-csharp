28f54b8610c3f7cc89870d0880091c3e4cd72bcd
ranicholson <ranicho2@gmail.com>
2021-07-27 18:17:19 +0000

    Task 5

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Class Zombie under the Enemies namespace.</summary>
+    class Zombie
+    {
+        /// <summary>Health field that has value assigned when new instance is initialized</summary>
+        private int health;
+
+        ///<summary>Name field that has string value and defaults to (No Name)</summary>
+        private string name = "(No name)";
+
+        /// <summary>Gets or sets the name</summary>
+        public string Name { get {return (name);} set {name=value;} }
+
+        ///<summary>Initializes new instance of Zombie class.</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        ///<summary>Initializes new instance of class Zombie with value to be assigned to health</summary>
+        ///<param name="value">Value to set health. Must be greater than or equal to zero.</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+        }
+
+        ///<summary>Returns the value of health</summary>
+        public int GetHealth()
+        {
+            return (this.health);
+        }
+
+        ///<summary>Overrides toString() and prints zombie attributes</summary>
+        public override string ToString()
+        {
+            return ("Zombie Name: " + name + " / Total Health: " + health);
+        } 
+    }
+}
