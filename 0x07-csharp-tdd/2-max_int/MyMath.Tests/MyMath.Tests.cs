6aa8b24bfa016f18048ebabdf3371d6642c9f0e4
ranicholson <ranicho2@gmail.com>
2021-07-29 11:48:16 -0500

    Task 2 content

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -1,18 +1,50 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace MyMath.Tests
 {
+    ///<summary>Class to test MyMath.Max()</summary>
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void PositiveNumbers()
         {
+            List<int> nums = new List<int> {0, 1, 2, 3, 4};
+            int maxNum = Operations.Max(nums);
+            Assert.AreEqual(4, maxNum);
         }
 
         [Test]
-        public void Test1()
+        public void NegativeNumbers()
         {
-            Assert.Pass();
+            List<int> nums = new List<int> {-1, -2, -3, -4};
+            int maxNum = Operations.Max(nums);
+            Assert.AreEqual(-1, maxNum);
+        }
+
+        [Test]
+        public void PositiveandNegativeNumbers()
+        {
+            List<int> nums = new List<int> {0, 1, 2, -3, -4};
+            int maxNum = Operations.Max(nums);
+            Assert.AreEqual(2, maxNum);
+        }
+
+        [Test]
+        public void AlltheSame()
+        {
+            List<int> nums = new List<int> {5, 5, 5, 5, 5};
+            int maxNum = Operations.Max(nums);
+            Assert.AreEqual(5, maxNum);
+        }
+
+        [Test]
+        public void EmptyList()
+        {
+            List<int> nums = new List<int> {};
+            int maxNum = Operations.Max(nums);
+            Assert.AreEqual(0, maxNum);
         }
     }
 }
--- 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MyMath
 {
-    public class Class1
+    ///<summary>Class that contains a method to return max integer</summary>
+    public class Operations
     {
+        ///<summary>Returns the max integer in a list of integers</summary>
+        ///<param name="nums">List with integers to find max of</param>
+        public static int Max(List<int> nums)
+        {
+            if (nums.Count == 0)
+            return (0);
+            return (nums.Max());
+        }
     }
 }
