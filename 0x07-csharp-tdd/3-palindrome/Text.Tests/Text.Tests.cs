f5ccfe42f2bf19af15e5fe9b75b4def1969c27cb
ranicholson <ranicho2@gmail.com>
2021-07-29 12:47:56 -0500

    Task 3 content

--- 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -1,18 +1,56 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Text.Tests
 {
+    ///<summary>Class to test Text.IsPalindrome()</summary>
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void easyTest()
         {
+            string s = "tattarrattat";
+            bool palindromeTest = Str.IsPalindrome(s);
+            Assert.AreEqual(true, palindromeTest);
+        }
+        [Test]
+        public void SpacesTest()
+        {
+            string s = " t a t t a r r a t t a t ";
+            bool palindromeTest = Str.IsPalindrome(s);
+            Assert.AreEqual(true, palindromeTest);
         }
 
         [Test]
-        public void Test1()
+        public void PunctionTest()
         {
-            Assert.Pass();
+            string s = "t?a.t,t'a;rrattat";
+            bool palindromeTest = Str.IsPalindrome(s);
+            Assert.AreEqual(true, palindromeTest);
+        }
+
+        [Test]
+        public void spacesandPuncTest()
+        {
+            string s = ",t .at t'a' rr' 'at't 'a t.,";
+            bool palindromeTest = Str.IsPalindrome(s);
+            Assert.AreEqual(true, palindromeTest);
+        }
+        [Test]
+        public void emptyTest()
+        {
+            string s = "";
+            bool palindromeTest = Str.IsPalindrome(s);
+            Assert.AreEqual(true, palindromeTest);
+        }
+
+        [Test]
+        public void betterFailTest()
+        {
+            string s = "NOT A PALINDROME";
+            bool palindromeTest = Str.IsPalindrome(s);
+            Assert.AreEqual(false, palindromeTest);
         }
     }
 }
--- 0x07-csharp-tdd/3-palindrome/Text/Text.cs
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -1,8 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Text
 {
-    public class Class1
+    ///<summary>Class containing method to check if string is a palindrome.</summary>
+    public class Str
     {
+        ///<summary>Returns true if string is a palindrome or false if not</summary>
+        ///<param name="s">String to check palindrominess of</param>
+        public static bool IsPalindrome(string s)
+        {
+            string newString = "";
+            string reverseString = "";
+            foreach (char character in s)
+            {
+                if (Char.IsPunctuation(character) == true || Char.IsWhiteSpace(character) == true)
+                    continue;
+                newString += Char.ToLower(character);
+            }
+            for (int x = newString.Length - 1;x >= 0; x--)
+                reverseString += newString[x];
+            if (newString == reverseString)
+                return (true);
+            return (false);    
+        }
     }
 }
