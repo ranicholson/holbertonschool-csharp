4b45a14804e4d521cbe3dd7b688c15dc1a4d6d4b
ranicholson <ranicho2@gmail.com>
2021-07-29 13:56:43 -0500

    Task 4 content

--- 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -1,18 +1,42 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Text.Tests
 {
+    ///<summary>Class to test Str.UniqueChar()</summary>
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void easyString()
         {
+            string s = "eeaasy";
+            int indexResult = Str.UniqueChar(s);
+            Assert.AreEqual(4, indexResult);
         }
 
         [Test]
-        public void Test1()
+        public void noUnique()
         {
-            Assert.Pass();
+            string s = "eeaassyy";
+            int indexResult = Str.UniqueChar(s);
+            Assert.AreEqual(-1, indexResult);
+        }
+
+        [Test]
+        public void firstUnique()
+        {
+            string s = "eaasy";
+            int indexResult = Str.UniqueChar(s);
+            Assert.AreEqual(0, indexResult);
+        }
+
+        [Test]
+        public void LastUnique()
+        {
+            string s = "eeaassy";
+            int indexResult = Str.UniqueChar(s);
+            Assert.AreEqual(6, indexResult);
         }
     }
 }
--- 0x07-csharp-tdd/4-unique/Text/Text.cs
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Text
 {
-    public class Class1
+    ///<summary>Class containing a method that returns indexCount offirst non-repeating character</summary>
+    public class Str
     {
+        ///<summary>Returns the first non-repeating character of a string</summary>
+        public static int UniqueChar(string s)
+        {
+            char amIUnique = s[0];
+            int indexCount = 0;
+
+            if (amIUnique != s[1])
+                return (indexCount);
+            
+            for (; indexCount < s.Length; indexCount++)
+            {
+                if (amIUnique == s[indexCount] && indexCount + 1 == s.Length)
+                {
+                    indexCount = -1;
+                    break;
+                }
+                if (amIUnique == s[indexCount])
+                    continue;
+
+                amIUnique = s[indexCount];
+                if (indexCount + 1 == s.Length)
+                    break;
+                if (amIUnique != s[indexCount+1])
+                    break;
+            }
+            return (indexCount);
+        }
     }
 }
