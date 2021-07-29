97b62f341746ab64adf4a9da24ec64b54ae3ab7e
ranicholson <ranicho2@gmail.com>
2021-07-29 15:13:41 -0500

    Task 5 content

--- 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -2,17 +2,40 @@ using NUnit.Framework;
 
 namespace Text.Tests
 {
+    ///<summary>Class to test Str.CamelCase()</summary>
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void easyTest()
         {
+            string s = "twoWords";
+            int wordCount = Str.CamelCase(s);
+            Assert.AreEqual(2, wordCount);
         }
 
         [Test]
-        public void Test1()
+        public void oneWord()
         {
-            Assert.Pass();
+            string s = "two";
+            int wordCount = Str.CamelCase(s);
+            Assert.AreEqual(1, wordCount);
+        }
+
+        [Test]
+        public void manyWords()
+        {
+            string s = "therAreSoManyWords";
+            int wordCount = Str.CamelCase(s);
+            Assert.AreEqual(5, wordCount);
+        }
+
+        [Test]
+        public void emptyString()
+        {
+            string s = "";
+            int wordCount = Str.CamelCase(s);
+            Assert.AreEqual(0, wordCount);
         }
     }
 }
--- 0x07-csharp-tdd/5-camelcase/Text/Text.cs
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -2,7 +2,22 @@
 
 namespace Text
 {
-    public class Class1
+    ///<summary>Class that contains method to determine how many words in a string</summary>
+    public class Str
     {
+        ///<summary>Determines how many words are in a string based by capital letter of each word</summary>
+        ///<param name = "s">String to count number of word in</param>
+        public static int CamelCase(string s)
+        {
+            if (s == "")
+                return (0);
+            int wordCount = 1;
+            foreach (char character in s)
+            {
+                if (Char.IsUpper(character) == true)
+                    wordCount += 1;
+            }
+            return (wordCount);
+        }
     }
 }
