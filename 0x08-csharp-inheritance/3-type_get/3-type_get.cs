4a64ee434673645b3263bea328ecd0f3f403de7e
ranicholson <ranicho2@gmail.com>
2021-08-02 13:41:52 -0500

    Task 3

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+///<summary>Class with a method</summary>
+class Obj
+{
+    ///<summary>Prints the names of available properties and methods of an object</summary>
+    public static void Print(object myObj)
+    {
+        TypeInfo infoList = myObj.GetType().GetTypeInfo();
+        Console.WriteLine("{0} Properties:", infoList.Name);
+
+        foreach (PropertyInfo item in infoList.GetProperties())
+            Console.WriteLine(item.Name);
+
+        Console.WriteLine("{0} Methods:", infoList.Name);
+
+        foreach (MethodInfo item in infoList.GetMethods())
+            Console.WriteLine(item.Name);
+    }
+}
