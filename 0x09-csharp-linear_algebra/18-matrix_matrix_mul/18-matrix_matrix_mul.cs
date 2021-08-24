b76e92e0be4b7c6c11f76eeb1ff7716a5d878833
ranicholson <ranicho2@gmail.com>
2021-08-24 15:19:28 -0500

    Task 17 and 18

---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+class MatrixMath
+{
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        double[,] prodMatrix;
+
+        prodMatrix = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+        for (int x = 0; x < matrix1.GetLength(0); x++)
+        {
+            for (int y = 0; y < matrix1.GetLength(0); y++)
+            {
+                for (int z = 0; z < matrix1.GetLength(0); z++)
+                    prodMatrix[x, y] += matrix1[x, z] * matrix2[z, y];
+            }
+        }
+        return (sumMatrix);
+    }
+}
