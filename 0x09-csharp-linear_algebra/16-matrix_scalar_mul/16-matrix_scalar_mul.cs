7ecc0d8eb204a50dc77e3b4028efb5ab4b265f85
ranicholson <ranicho2@gmail.com>
2021-08-24 12:00:50 -0500

    Task 16 updates

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -9,18 +9,18 @@ class MatrixMath
     {
         double[,] prodMatrix;
 
-        if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.Length != matrix2.Length || matrix1.GetLength(0) < 2)
+        if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3)
             return (new double [,] {{-1}});
 
-        if (matrix1.GetLength(0) == 2)
+        if (matrix.GetLength(0) == 2)
             prodMatrix  = new double[2,2];
         else
             prodMatrix = new double[3,3];
 
-        for (int x = 0; x < matrix1.GetLength(0); x++)
+        for (int x = 0; x < matrix.GetLength(0); x++)
         {
-            for (int y = 0; y < matrix1.GetLength(0); y++)
-                prodMatrix[x, y] = matrix1[x, y] * scalar;
+            for (int y = 0; y < matrix.GetLength(0); y++)
+                prodMatrix[x, y] = matrix[x, y] * scalar;
         }
 
         return (prodMatrix);
