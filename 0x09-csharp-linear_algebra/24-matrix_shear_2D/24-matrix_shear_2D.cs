fe548c2f63e0b89b40cad9dbfa3ae3bcfe35e55d
ranicholson <ranicho2@gmail.com>
2021-08-26 13:24:11 -0500

    Task 24

---
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+/// <summary>Class containing matrix math</summary>
+class MatrixMath
+{
+    /// <summary>Method that shears a 2D matrix by the providedshear factor.</summary>
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        if (direction == "x" && matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+            return (new double[,] {{matrix[0,0] + (factor * matrix[0, 0])}, {matrix[1, 0] + (factor * matrix[1, 1]), matrix[1 ,1]}});
+
+        if (direction == "y" && matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+            return (new double[,] {{matrix[0, 0], matrix[0, 1] + (factor * matrix[0, 0])}, {matrix[1, 0], matrix[1, 1] + (factor * matrix[1, 0])}});
+
+        return (new double[,] {{-1}});
+    }
+}
