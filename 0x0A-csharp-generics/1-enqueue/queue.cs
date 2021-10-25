1a39fbaaf5a4fffb408e4d2be6b04a5b1a42002b
ranicholson <ranicho2@gmail.com>
2021-10-25 12:37:28 -0500

    Task 1

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+///<summary>Generic Queue class to do different things</summary>
+class Queue<T>
+{
+    int count = 0;
+    Node head;
+    Node tail;
+
+    ///<summary>Count method that returns number of nodes in Queue</summary>
+    public int Count()
+    {
+        return (count);
+    }
+
+    ///<summary>Checks what type the Queue is</summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    ///<summary>Creates a new node and adds it to the end of the queue</summary>
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        count++;
+    }
+
+    ///<summary>Public class Node inside Queue class</summary>
+    public class Node
+    {
+        public T value = default(T);
+        public Node next = null;
+
+    ///<summary>Constructor that takes a value for a new node and sets it</summary>
+        public Node(T value)
+        {
+            this.value = value;
+        }
+    }
+}
