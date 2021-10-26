c1836dda095ed9605546f37d1121c141321c8e8e
ranicholson <ranicho2@gmail.com>
2021-10-26 13:50:12 -0500

    Task 1

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+///<summary>Abstract base class</summary>
+
+abstract class Base
+{
+    public string name = "";
+
+    ///<summary>Override of ToString() that returns name and type</summary>
+    public override string ToString()
+    {
+        return (this.name +" is a " + this.GetType().ToString());
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Interact()
+	{
+        
+	}
+
+    public void Break()
+	{
+
+	}
+
+    public void Collect()
+	{
+
+	}
+}
