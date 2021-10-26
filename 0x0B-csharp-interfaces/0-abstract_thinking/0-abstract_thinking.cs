9f64c5cfc14a67d8191e96ae894696c3df164a14
ranicholson <ranicho2@gmail.com>
2021-10-26 13:06:46 -0500

    Task 0 and README

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+///<summary>Abstract base class</summary>
+
+abstract class Base
+{
+    public string name = "";
+
+    ///<summary>Override of ToString() that returns name and type</summary>
+    public override string ToString()
+    {
+        return (this.name +" is a" + this.GetType().ToString());
+    }
+}
