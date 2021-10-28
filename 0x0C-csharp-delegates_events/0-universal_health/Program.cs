05268ac30d680b0c0f7b973964653c94d2e042e2
ranicholson <ranicho2@gmail.com>
2021-10-28 15:15:29 -0500

    Fixing negative HP problem

--- 0x0C-csharp-delegates_events/0-universal_health/Program.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/Program.cs
@@ -12,7 +12,7 @@ class Player
     ///<summary>Constructor for class</summary>
     public Player(string name="Player", float maxHp=100f)
     {
-        if (maxHp <= 0)
+        if (maxHp <= 0f)
         {
             this.maxHp = 100f;
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
