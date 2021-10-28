e810442da461e6f2bfad1388aed3a835f102118d
ranicholson <ranicho2@gmail.com>
2021-10-28 15:14:17 -0500

    Task 0 and README

---
+++ 0x0C-csharp-delegates_events/0-universal_health/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+///<summary>Creates public Player class</summary>
+
+class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    ///<summary>Constructor for class</summary>
+    public Player(string name="Player", float maxHp=100f)
+    {
+        if (maxHp <= 0)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+
+        else
+            this.maxHp = maxHp;
+
+        this.name = name;
+        this.hp = maxHp;
+    }
+
+    ///<summary>Prints hp infdormation</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
---
+++ 0x0C-csharp-delegates_events/README.md
@@ -0,0 +1,11 @@
+# 0x0C. C# - Delegates, Events
+
+Delegates events in C#
+
+## How can you practice this same project?
+
+Take a look at the AR/VR specialization at Holberton.
+
+## Questions or Comments
+
+[Allen Nicholson](https://github.com/ranicholson)
