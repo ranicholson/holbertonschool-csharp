b98d8ac1b8c27f20f3ca2cda7dcdedc1fb8597ad
ranicholson <ranicho2@gmail.com>
2021-11-02 12:54:12 -0500

    Task 0 and README

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+///<summary>BaseClass that other classes will inherit from</summary>
+class BaseClass
+{
+    public string id;
+    public DateTime date_created;
+    public DateTime date_updated;
+
+    ///<summary>Constructor for BaseClass</summary>
+    public BaseClass()
+    {
+        this.id = Guid.NewGuid().ToString();
+        this.date_created = DateTime.Now;
+        this.date_updated = this.date_created;
+    }
+}
---
+++ 0x0D-csharp-text_based_interface/README.md
@@ -0,0 +1,11 @@
+# 0x0D. C# - Text-based Interface
+
+Creating a text based interface in c#
+
+## How can you practice this same project?
+
+Take a look at the AR/VR specialization at Holberton.
+
+## Questions or Comments
+
+[Allen Nicholson](https://github.com/ranicholson)
